32f4d9de97e0882636b59f66b87e73e418e8de54
benweiser <bweiser86@gmail.com>
2019-03-21 23:24:53 -0700

    Covert SearchOptions, Icon, and CurrentWeatherIcon to ts

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
 import '../styles/icons/weather-icons.min.css';
 import styled from 'react-emotion';
 
-const iconSizes = {
+export interface IconProps {
+  name: string;
+  size: string;
+}
+
+const iconSizes: { [key: string]: string } = {
   sm: '24px',
   md: '32px',
   lg: '48px',
   xl: '64px'
 };
 
-const StyledIcon = styled('span')`
+const StyledIcon = styled('span')<{ size: string }>`
   font-size: ${props => iconSizes[props.size] || props.size};
 `;
 
-const Icon = ({ name, ...props }) => {
+const Icon = ({ name, ...props }: IconProps) => {
   return <StyledIcon className={`wi ${name}`} {...props} />;
 };
 
 export default Icon;
-
-StyledIcon.propTypes = {
-  size: PropTypes.string.isRequired
-};
